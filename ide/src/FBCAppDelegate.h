cb9b796d6b49feface088469991dd4aba90c97cc
Jakub Olejnik <olejnjak@gmail.com>
2014-03-10 19:03:16 +0100

    Vytvoren prazdny Xcode projekt.

---
+++ ide/src/FBCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FBCAppDelegate.h
+//  Floorbal Coach
+//
+//  Created by Jakub Olejnik on 10/03/2014.
+//  Copyright (c) 2014 Jakub Olejník. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
