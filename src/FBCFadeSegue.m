2cfe6636ecc818d7f017c5df7e0fc8229daed9c2
Jakub Olejnik <olejnjak@gmail.com>
2014-03-12 14:36:47 +0100

    Upravena fade segue.

--- src/FBCFadeSegue.m
+++ src/FBCFadeSegue.m
@@ -13,38 +13,20 @@
 - (void)perform
 {
     UIViewController *src = self.sourceViewController;
+    UIViewController *dst = self.destinationViewController;
+    UIWindow *appWindow = src.view.window;
     
     [self prepareDestinationController];
     
-    [UIView animateWithDuration:0.3 animations:^{
-        [src.view setAlpha:0.0];
-    } completion:^(BOOL finished) {
-        [self changeViewController];
-        [self showDestinationController];
-    }];
-}
-
-- (void)changeViewController
-{
-    UIViewController *src = self.sourceViewController;
-    UIViewController *dst = self.destinationViewController;
-    
-    UIWindow *appWindow = src.view.window;
-    [appWindow performSelectorOnMainThread:@selector(setRootViewController:)
-                                withObject:dst
-                             waitUntilDone:NO];
-}
-
-- (void)showDestinationController
-{
-    UIViewController *src = self.sourceViewController;
-    UIViewController *dst = self.destinationViewController;
-    
-    [UIView animateWithDuration:0.3 animations:^{
-        [dst.view setAlpha:1.0];
-    } completion:^(BOOL finished) {
-        [src.view removeFromSuperview];
-    }];
+    [UIView transitionFromView:src.view toView:dst.view duration:0.3
+                       options:UIViewAnimationOptionTransitionCrossDissolve completion:^(BOOL finished)
+     {
+         [appWindow performSelectorOnMainThread:@selector(setRootViewController:)
+                                     withObject:dst
+                                  waitUntilDone:NO];
+         
+         [src.view removeFromSuperview];
+     }];
 }
 
 - (void)prepareDestinationController
@@ -52,7 +34,6 @@
     UIViewController *src = self.sourceViewController;
     UIViewController *dst = self.destinationViewController;
     
-    [dst.view setAlpha:0.0];
     dst.view.center = src.view.center;
     dst.view.transform = src.view.transform;
     dst.view.bounds = src.view.bounds;
